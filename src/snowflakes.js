fad7ca355e69b82bcd74f32116f92a5654d04d4d
Karan Jit Singh <karan1866@gmail.com>
2015-12-22 23:35:57 +0530

    Organized Code. Added Comments.

--- src/snowflakes.js
+++ src/snowflakes.js
@@ -1,14 +1,17 @@
 var snowflakes = {
 	params: {
-		FlakeNumber: 30,
-		Size: 20,
-		SizeVariation: 10,
-		Velocity: 8,
-		VelocityVariation: 4,
-		VelocityFactor: 100,
+		Flakes: {
+			Count: 30,
+			Size: 20,
+			SizeVariation: 10,
+			Velocity: 8,
+			VelocityVariation: 4,
+			VelocityFactor: 100
+		},
 		WindDirection: "alternate", //["left", "right", "alternate", "none"]
 		WindVelocity: 4,
 		WindSwingTime: 4000,
+		FlakeImageUrl: "flake.svg"
 	},
 
 	flakes: [],
@@ -37,10 +40,14 @@ var snowflakes = {
 	},
 
 	randomizeFlake: function(flake) {
-		flake.velocity = (this.params.Velocity -this.params.VelocityVariation + Math.random()*(2*this.params.VelocityVariation))/this.params.VelocityFactor;
-		flake.size = (this.params.Size-this.params.SizeVariation) + Math.random()*(2 * this.params.SizeVariation);
+		//Random value in [Vmin, Vmax]
+		//Velocity is divided by scaling factor for smoothing/scalable units
+		flake.velocity = (this.params.Flakes.Velocity -this.params.Flakes.VelocityVariation + Math.random()*(2*this.params.Flakes.VelocityVariation))/this.params.Flakes.VelocityFactor;
+		flake.size = (this.params.Flakes.Size-this.params.Flakes.SizeVariation) + Math.random()*(2 * this.params.Flakes.SizeVariation);
 		flake.x = document.body.clientWidth * Math.random();
-		flake.y = (-1 * (this.params.Size + this.params.SizeVariation) - Math.random()*100);
+
+		//Keep the flake above client window. Keeps flake from randomly popping on screen when randomized
+		flake.y = (-1 * (this.params.Flakes.Size + this.params.Flakes.SizeVariation) - Math.random()*100);
 		flake.rotate = 60 * Math.random();
 	},
 
@@ -50,6 +57,8 @@ var snowflakes = {
 
 		this.flakeContainer = document.getElementsByClassName("snowflakes")[0];
 
+
+		//Use the same canvas if it exists in code else create a new one
 		if(!this.flakeContainer || this.flakeContainer.tagName != "CANVAS") {
 			this.flakeContainer = document.createElement("canvas");
 			this.flakeContainer.className = "snowflakes";
@@ -73,17 +82,21 @@ var snowflakes = {
 			snowflakes.flakeImage.loaded = true;
 		}
 
-		this.flakeImage.src = "flake.svg";
-		for(var i=0; i<this.params.FlakeNumber; i++)
+		this.flakeImage.src = this.params.FlakeImageUrl;
+		for(var i=0; i<this.params.Flakes.Count; i++)
 		{
 			var flake = new Object();
 			this.randomizeFlake(flake);
+
+			//Scatter flakes throughout window for more randomness. Decreases clustering on init
 			flake.y = Math.random() * this.flakeContainer.height;
 			this.flakes.push(flake);
 		}
 		this.state = "initialized";
 	},
 
+
+	//Canvas image draw
 	drawFlake: function(flake) {
 		var ctx = snowflakes.flakeContainer.getContext("2d");
 		ctx.save();
@@ -98,6 +111,7 @@ var snowflakes = {
 		if(snowflakes.state != "running")
 			return;
 
+		//To update velocity according to frame rate
 		var time = snowflakes.time;
 		if(!time.start){time.start=currentTime;}
 			if(!time.last){time.last=currentTime;}
@@ -109,15 +123,18 @@ var snowflakes = {
 		ctx.clearRect(0,0,parseInt(snowflakes.flakeContainer.width),parseInt(snowflakes.flakeContainer.height));
 
 		for(var i =0;i<snowflakes.flakes.length;i++) {
-			var top = (parseFloat(snowflakes.flakes[i].y) + snowflakes.flakes[i].velocity * delta);
-			var left = (parseFloat(snowflakes.flakes[i].x) + snowflakes.params.WindVelocity * delta / snowflakes.params.VelocityFactor * (0.5 + 0.5 * Math.random()) * snowflakes.windDirection());
 
+			//pos = pos + v * delta
+			var top = (parseFloat(snowflakes.flakes[i].y) + snowflakes.flakes[i].velocity * delta);
+			var left = (parseFloat(snowflakes.flakes[i].x) + snowflakes.params.WindVelocity * delta / snowflakes.params.Flakes.VelocityFactor * (0.5 + 0.5 * Math.random()) * snowflakes.windDirection());
+
+			//Check if flake has crossed the bottom, if yes re-randomize flake
 			if(top<= document.body.clientHeight){
 				snowflakes.flakes[i].y = top;
 				snowflakes.flakes[i].x = left;
 			}
 			else
-				snowflakes.randomizeFlake(snowflakes.flakes[i]);
+				snowflakes.randomizeFlake(snowflakes.flakes[i]);		//Re-randomize flake
 
 			snowflakes.drawFlake(snowflakes.flakes[i]);
 		}
@@ -128,7 +145,7 @@ var snowflakes = {
 
 	stop: function() {
 		snowflakes.flakeContainer.style.opacity = 0;
-		setTimeout(snowflakes.stopCallback, 500);
+		setTimeout(snowflakes.stopCallback, 500);	//Stop status is reflected after fadeOut animation is complete
 	},
 
 	stopCallback: function() {
@@ -139,13 +156,15 @@ var snowflakes = {
 	start: function() {
 		this.init();
 		this.flakeContainer.style.display = "initial";
+
+		//If flake image has loaded then start the animation, else wait for loading to finish
 		if(this.flakeImage && this.flakeImage.loaded) {
 			this.flakeContainer.style.opacity = 1;
 			this.state="running";
 			window.requestAnimationFrame(snowflakes.nextFrame);
 		}
 		else if(this.flakeImage) {
-			this.state = "waitingForSvg";
+			this.state = "waitingForImage";
 			this.flakeImage.onload = function() {
 				snowflakes.flakeImage.loaded = true;
 				snowflakes.flakeContainer.style.opacity = 1;
@@ -160,7 +179,7 @@ var snowflakes = {
 		{
 			var flake = this.flakes[i];
 			this.randomizeFlake(flake);
-			var top = Math.random() * this.flakeContainer.clientHeight;
+			var top = Math.random() * this.flakeContainer.height;
 			flake.y = top;
 		}
 	}
